20d21dbe81345b75ba903108ef255b6660f6d17b
nkct <miloszzegota@gmail.com>
2025-02-17 00:48:37 +0100

    implement viewsets for users and hydroponics

--- lunasci/hydroponics/views.py
+++ lunasci/hydroponics/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import permissions, viewsets
+
+from django.contrib.auth.models import User
+from lunasci.hydroponics.models import Hydroponics
+
+from lunasci.hydroponics.serializers import HydroponicsSerializer, UserSerializer
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+class HydroponicsViewSet(viewsets.ModelViewSet):
+    queryset = Hydroponics.objects.all().order_by("-created")
+    serializer_class = HydroponicsSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
