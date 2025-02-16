656c1222975b37b32a739e530842568f50aaf148
nkct <miloszzegota@gmail.com>
2025-02-17 00:51:26 +0100

    clean up namespaces

--- lunasci/hydroponics/models.py
+++ lunasci/hydroponics/models.py
@@ -6,3 +6,5 @@ class Hydroponics(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     owner = models.ForeignKey('auth.User', related_name='hydroponics', on_delete=models.CASCADE)
     
+    class Meta:
+        db_table = 'hydroponics'
