79956132a1be4cf4d568e838d9fb7c20cac8dd20
nkct <miloszzegota@gmail.com>
2025-02-17 00:50:00 +0100

    setup settings for DRF

--- lunasci/settings.py
+++ lunasci/settings.py
@@ -40,6 +40,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    'rest_framework',
+    'lunasci.hydroponics',
 ]
 
 MIDDLEWARE = [
@@ -130,3 +133,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Django REST Framework Settings
+# https://www.django-rest-framework.org/api-guide/settings/
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
