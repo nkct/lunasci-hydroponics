0212fbb6962bc27349137447e9d2d2efb2765a9a
nkct <miloszzegota@gmail.com>
2025-02-17 00:23:49 +0100

    initial Hydroponics model

--- lunasci/hydroponics/models.py
+++ lunasci/hydroponics/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Hydroponics(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    owner = models.ForeignKey('auth.User', related_name='hydroponics', on_delete=models.CASCADE)
+    
