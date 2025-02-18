eb1b9be79285729df1b5961841c515a52293cda6
Miłosz Żegota <miloszzegota@gmail.com>
2025-02-18 11:58:21 +0100

    implement SensorReading model

--- lunasci/hydroponics/models.py
+++ lunasci/hydroponics/models.py
@@ -8,3 +8,13 @@ class Hydroponics(models.Model):
     
     class Meta:
         db_table = 'hydroponics'
+
+class SensorReading(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    hydroponics = models.ForeignKey(Hydroponics, related_name='readings', on_delete=models.CASCADE)
+    ph = models.FloatField(null=True, blank=True)
+    temperature = models.FloatField(null=True, blank=True)
+    tds = models.FloatField(null=True, blank=True)
+    
+    class Meta:
+        db_table = 'sensor_reading'
